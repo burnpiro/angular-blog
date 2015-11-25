5ab13e9c4c37a1d5be5eb7e6ae6d3ea7912c89c5
Codeship CI <contact@codeship.com>
2015-11-25 18:02:36 +0000

    Left slider works :)

--- app/components/services/leftSlider.service.js
+++ app/components/services/leftSlider.service.js
@@ -1,23 +1,31 @@
 'use strict';
 
 angular.module('app.services').
-    service('LeftSliderService', ['$rootScope', '$templateCache', LeftSliderService]);
+    service('LeftSliderService', ['$rootScope', '$templateRequest', '$compile', LeftSliderService]);
 
-function LeftSliderService($rootScope, $templateCache) {
+function LeftSliderService($rootScope, $templateRequest, $compile) {
     var self = this;
 
-    var currentCtrl = '',
-        currentTemplate = '';
+    var currentTemplate = '';
 
     self.toggle = function(forceValue) {
-        console.log('aaaa');
         $rootScope.leftSlideOpen = angular.isDefined(forceValue) ? forceValue : !$rootScope.leftSlideOpen;
     };
 
-    self.loadTemplate = function(template, controller, forceRecompile) {
-        console.log('bbb');
-        console.log($templateCache.get(template));
+    self.loadTemplate = function(template, scope, forceRecompile) {
+        var leftSlider = angular.element(document.getElementById('leftSlider'));
+        $templateRequest(template).then(function(html) {
+            if(currentTemplate === template && !forceRecompile) {
+                self.toggle();
+                return false;
+            }
 
-        self.toggle(true);
+            currentTemplate = template;
+            leftSlider.html('');
+            var template = angular.element(html);
+            leftSlider.append(template);
+            $compile(template)(angular.isDefined(scope) ? scope : $rootScope);
+            self.toggle(true);
+        });
     }
 }
