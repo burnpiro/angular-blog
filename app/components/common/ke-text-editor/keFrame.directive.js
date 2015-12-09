1dcc1966305906449f07e73040e4ddbeee8a0a16
Codeship CI <contact@codeship.com>
2015-12-09 17:54:40 +0000

    Begin of text editor

--- app/components/common/ke-text-editor/keFrame.directive.js
+++ app/components/common/ke-text-editor/keFrame.directive.js
@@ -19,16 +19,21 @@ function keFrameDirective() {
     }
 
     function linkFn(scope, $element, attrs, ctrl) {
+
+        // Get iframe and put new HTML document inside
+        console.log($element[0].clientWidth);
         var editor = $element[0].contentDocument;
         editor.open();
-        editor.write('<!DOCTYPE html><html><head></head><body contenteditable="true"></body></html>');
+        editor.write('<!DOCTYPE html><html><head><style>img{ max-width: '+($element[0].clientWidth)+'px; }</style></head><body contenteditable="true"></body></html>');
         editor.close();
         editor.designMode = 'On';
+
+        // get body and head into variables
         var body = angular.element($element[0].contentDocument.body);
         var head = angular.element($element[0].contentDocument.head);
 
+        // Put ngModel into iframe body
         ctrl.render = function() {
-            console.log(ctrl);
             body[0].innerHTML = ctrl.ngModel;
         };
 
