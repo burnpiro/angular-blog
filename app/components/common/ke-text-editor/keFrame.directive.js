0969a83992ccdca44aa0dfbe6c0cbf47a0f4fbdb
Codeship CI <contact@codeship.com>
2015-12-22 17:37:54 +0000

    Text Editor (except image browser)

--- app/components/common/ke-text-editor/keFrame.directive.js
+++ app/components/common/ke-text-editor/keFrame.directive.js
@@ -1,7 +1,7 @@
 angular.module('app.common').
-    directive('keFrame', [keFrameDirective]);
+    directive('keFrame', ['$timeout', keFrameDirective]);
 
-function keFrameDirective() {
+function keFrameDirective($timeout) {
     return {
         scope: true,
         require: '^keTextEditor',
@@ -15,8 +15,14 @@ function keFrameDirective() {
         link: linkFn
     };
 
-    function ctrl() {
+    function ctrl($scope) {
         var self = this;
+
+        self.sync = function() {
+            $scope.$evalAsync(function($scope) {
+                self.ngModel.$setViewValue(body.html());
+            });
+        }
     }
 
     function linkFn(scope, $element, attrs, ctrl) {
@@ -25,7 +31,7 @@ function keFrameDirective() {
         // Get iframe and put new HTML document inside
         var editor = $element[0].contentDocument;
         editor.open();
-        editor.write('<!DOCTYPE html><html><head><style>img{ max-width: '+($element[0].clientWidth)+'px; }</style></head><body contenteditable="true"></body></html>');
+        editor.write('<!DOCTYPE html><html><head><style>iframe{background: repeating-linear-gradient(45deg, #689F38, #689F38 10px, #c6c6c6 10px, #c6c6c6 20px)} img{ max-width: '+($element[0].clientWidth)+'px; }</style></head><body contenteditable="true"></body></html>');
         editor.close();
         editor.designMode = 'On';
 
@@ -38,8 +44,47 @@ function keFrameDirective() {
             body[0].innerHTML = ctrl.ngModel;
         };
 
-        ctrl.executeCommand = function(cmd) {
+        scope.sync = function() {
+            scope.$evalAsync(function(scope) {
+                ctrl.ngModel = body.html();
+            });
+        };
+
+        var timeout = null;
+        body.bind('click keyup change paste', function() {
+            if(timeout) {
+                $timeout.cancel(timeout);
+            }
+
+            timeout = $timeout(function() {
+                scope.sync();
+            }, 300)
+        });
+
+        ctrl.executeCommand = function(cmd, args) {
             body.focus();
+
+            var selection = editor.getSelection();
+            if(selection) {
+                if(angular.isObject(args)) {
+                    if(angular.isDefined(args.end)) {
+                        editor.execCommand(cmd, false, args.begin+selection+args.end);
+                    } else {
+                        editor.execCommand(cmd, false, args.begin);
+                    }
+                } else {
+                    editor.execCommand(cmd, false, args);
+                }
+            } else {
+                editor.execCommand(cmd, false, args);
+            }
+
+            editor.body.focus();
+            scope.sync();
+        };
+
+        ctrl.getSelection = function() {
+            return editor.getSelection();
         };
 
         ctrl.render();
