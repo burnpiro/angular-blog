3e6b74acacb5ba29b4b2b420a730e4e6df4398ed
Codeship CI <contact@codeship.com>
2016-01-22 17:43:27 +0000

    create server config

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express        = require('express');
 var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-
-var port = process.env.PORT || 3000; // set our port
-var staticdir = process.env.NODE_ENV === 'production' ? 'dist.prod' : 'dist.dev'; // get static files dir
+var config         = require('./serverConfig');
 
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json
@@ -12,16 +10,16 @@ app.use(bodyParser.json()); // parse application/json
 //app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + '/' + staticdir)); // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/' + config.staticDir)); // set the static files location /public/img will be /img for users
 
 // routes ==================================================
-require('./devServer/routes')(app, staticdir); // configure our routes
+require('./devServer/routes')(app, config.staticDir); // configure our routes
 
 // This route deals enables HTML5Mode by forwarding missing files to the index.html
 app.all('/*', function(req, res) {
-    res.sendFile(__dirname + '/' +staticdir+'/index.html');
+    res.sendFile(__dirname + '/' +config.staticDir+'/index.html');
 });
 // start app ===============================================
-app.listen(port);                   // startup our app at http://localhost:8080
-console.log('Starting sever on port ' + port);       // shoutout to the user
+app.listen(config.server.port);                   // startup our app at http://localhost:8080
+console.log('Starting sever on port ' + config.server.port);       // shoutout to the user
 exports = module.exports = app;             // expose app
---
+++ serverConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    product: {
+        name: 'Hapi Blog API'
+    },
+    server: {
+        host: 'localhost',
+        port: 3000
+    },
+    staticDir: 'dist.dev'
+};
